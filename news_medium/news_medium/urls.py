fb5a174700e2ee2779802f449c8fdfe16b8e34df
Yan <bbb.zzzz.bbb@mail.ru>
2022-06-09 17:03:40 +0200

    fix static

--- news_medium/news_medium/urls.py
+++ news_medium/news_medium/urls.py
@@ -5,13 +5,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('news.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('news.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns = [
                       path('__debug__/', include(debug_toolbar.urls)),
                   ] + urlpatterns
 
-    urlpatterns += static(settings.MEDIA_URL, document_toot=settings.MEDIA_ROOT)
